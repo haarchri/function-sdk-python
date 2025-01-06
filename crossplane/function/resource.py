17507825e72c89a48e419265c8dbf739e91c6f8a
Christopher Haar <christopher.haar@upbound.io>
2025-01-06 16:05:25 +0100

    fix(defaults): add apiVersion and kind
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- crossplane/function/resource.py
+++ crossplane/function/resource.py
@@ -38,7 +38,10 @@ def update(r: fnv1.Resource, source: dict | structpb.Struct | pydantic.BaseModel
     """
     match source:
         case pydantic.BaseModel():
-            r.resource.update(source.model_dump(exclude_defaults=True, warnings=False))
+            data = source.model_dump(exclude_defaults=True, warnings=False)
+            data['apiVersion'] = source.apiVersion
+            data['kind'] = source.kind
+            r.resource.update(data)
         case structpb.Struct():
             # TODO(negz): Use struct_to_dict and update to match other semantics?
             r.resource.MergeFrom(source)
